9a48ca845a3a53186bf7ab5fc0f5f7cc75bc463e
Artyom Shikovetsky <tera1134@gmail.com>
2020-09-30 20:41:05 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2)
+    return false;
+
+  let splitted = str.split('');
+
+  for (let i = 0; i < splitted.length; i++) {
+    for (let config of bracketsConfig) {
+      if (config[0] === splitted[i] && config[1] === splitted[i + 1])
+        return check(splitted.slice(0, i).concat(splitted.slice(i + 2)).join(''), bracketsConfig);
+    }
+  }
+
+  return !splitted.length;
 }
